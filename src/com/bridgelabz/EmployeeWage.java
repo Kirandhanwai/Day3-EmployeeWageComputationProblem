e9d84990c42aa1f42fd65486c19f58f2c77f3c3c
Kirandhanwai <kirandhanwai123@gmail.com>
2022-09-29 00:48:59 +0530

    Addded: Calculating Employee 20 day working day per mouth

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,12 +6,15 @@ public class EmployeeWage {
 	static final int PART_PER_HOUR = 4;
 	static final int IS_FULL_TIME = 2;
 	static final int IS_PART_TIME = 1;
+	static final int WORKING_DAY_PER_MONTH = 20;
 	
 	public static void main(String[] arg) {
-		
+		int day =1;
+		int totalWage =0;
+	while (day<=WORKING_DAY_PER_MONTH)	{
+		int dailyWage =0;
 		int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
 			
-		int dailyWage = 0;
 	switch (employeeAttendance)
 	{
 		case IS_FULL_TIME :
@@ -25,10 +28,13 @@ public class EmployeeWage {
 		default:
 			System.out.println("Employee is absent");
 		}
-	System.out.println("Daily Wage =>" + dailyWage);
+	totalWage=totalWage + dailyWage;
+	System.out.println("Daily Wage for=>" + day + "day =>" + dailyWage);
+	day++;
+	}
+	System.out.println("Total Wage =>" + totalWage);
 	}
 }
-
 
 
 
