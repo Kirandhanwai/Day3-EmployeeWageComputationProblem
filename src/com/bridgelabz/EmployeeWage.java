93d0cd286ede39e2872c726fc52db557fd397e32
Kirandhanwai <kirandhanwai123@gmail.com>
2022-09-28 16:43:04 +0530

    Added: Employee Attendance Program

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,8 +3,13 @@ package com.bridgelabz;
 public class EmployeeWage {
 	
 	public static void main(String[] arg) {
-		// Welcome to Employee Wage Problem
-		System.out.println("Welcome to employee wage problem");
+		int employeeAttendance=(int)Math.round(Math.random());
+			System.out.println(employeeAttendance);
+		if (employeeAttendance == 1) {
+			System.out.println("Employee is Present");
 		}
-
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
 }
