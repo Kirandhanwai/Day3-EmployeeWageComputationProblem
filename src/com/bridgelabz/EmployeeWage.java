996c3114b8a21cf3e5c3c6f304d12a18dd17e884
Kirandhanwai <kirandhanwai123@gmail.com>
2022-09-29 01:14:55 +0530

    Addded: Calculating Employee Wage total working hours or days is reached for a mounth

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,16 +2,18 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 	static final int WAGE_PER_HOUR = 20;
-	static final int FULL_PER_HOUR = 8;
-	static final int PART_PER_HOUR = 4;
+	static final int FULL_TIME_HOUR = 12;
+	static final int PART_TIME_HOUR = 6;
 	static final int IS_FULL_TIME = 2;
 	static final int IS_PART_TIME = 1;
 	static final int WORKING_DAY_PER_MONTH = 20;
+	static final int WORKING_HOURS_PER_MONTH = 100;
 	
 	public static void main(String[] arg) {
 		int day =1;
 		int totalWage =0;
-	while (day<=WORKING_DAY_PER_MONTH)	{
+		int empHours =0;
+	while (day<=WORKING_DAY_PER_MONTH && empHours <=WORKING_HOURS_PER_MONTH)	{
 		int dailyWage =0;
 		int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
 			
@@ -19,11 +21,13 @@ public class EmployeeWage {
 	{
 		case IS_FULL_TIME :
 			System.out.println("Employee is full time");
-			dailyWage= WAGE_PER_HOUR*FULL_PER_HOUR;
+			dailyWage= WAGE_PER_HOUR*FULL_TIME_HOUR;
+			empHours = empHours + FULL_TIME_HOUR;
 			break;
 		case IS_PART_TIME:
 			System.out.println("Employee is part time");
-			dailyWage= WAGE_PER_HOUR*PART_PER_HOUR;
+			dailyWage= WAGE_PER_HOUR*PART_TIME_HOUR;
+			empHours = empHours + PART_TIME_HOUR;
 			break;
 		default:
 			System.out.println("Employee is absent");
@@ -32,6 +36,7 @@ public class EmployeeWage {
 	System.out.println("Daily Wage for=>" + day + "day =>" + dailyWage);
 	day++;
 	}
+	System.out.println("Total Working Hours =>"+ empHours);
 	System.out.println("Total Wage =>" + totalWage);
 	}
 }
