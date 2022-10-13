af457f878335465b0c8e8eaf1b42c6a1517c0bd3
Kirandhanwai <kirandhanwai123@gmail.com>
2022-10-13 23:40:08 +0530

    Added : Refactored the Code

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class EmployeeWage {
+class EmployeeWage {
 	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_TIME_HOUR = 12;
 	static final int PART_TIME_HOUR = 6;
@@ -9,37 +9,57 @@ public class EmployeeWage {
 	static final int WORKING_DAY_PER_MONTH = 20;
 	static final int WORKING_HOURS_PER_MONTH = 100;
 	
-	public static void main(String[] arg) {
-		int day =1;
-		int totalWage =0;
-		int empHours =0;
-	while (day<=WORKING_DAY_PER_MONTH && empHours <=WORKING_HOURS_PER_MONTH)	{
-		int dailyWage =0;
+	public static int empCheck() {
 		int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
-			
-	switch (employeeAttendance)
-	{
-		case IS_FULL_TIME :
-			System.out.println("Employee is full time");
-			dailyWage= WAGE_PER_HOUR*FULL_TIME_HOUR;
-			empHours = empHours + FULL_TIME_HOUR;
-			break;
-		case IS_PART_TIME:
-			System.out.println("Employee is part time");
-			dailyWage= WAGE_PER_HOUR*PART_TIME_HOUR;
-			empHours = empHours + PART_TIME_HOUR;
-			break;
-		default:
-			System.out.println("Employee is absent");
+		return employeeAttendance;
+	    }
+	
+	public static int empHours(int empCheck) {
+		int empHours;
+		switch (empCheck) {
+		case IS_FULL_TIME -> empHours = FULL_TIME_HOUR;
+		case IS_PART_TIME -> empHours = PART_TIME_HOUR;
+		default -> empHours = 0;
 		}
-	totalWage=totalWage + dailyWage;
-	System.out.println("Daily Wage for=>" + day + "day =>" + dailyWage);
-	day++;
-	}
-	System.out.println("Total Working Hours =>"+ empHours);
-	System.out.println("Total Wage =>" + totalWage);
-	}
+		return empHours;
+	    }
+	
+	public static int empWage(int empHours) {
+		int empWage = empHours * WAGE_PER_HOUR;
+		return empWage;
+	    }
+	
+	  public static int totalEmpSalary() {
+			int day = 1;
+			int totalEmpSalary = 0;
+			int empHours = 0;
+			int empWage = 0;
+			int empCheck = 0;
+			int totalempHours = 0;
+			while (day <= WORKING_DAY_PER_MONTH && totalempHours < WORKING_HOURS_PER_MONTH) {
+			    empCheck = empCheck();
+			    if (empCheck == 0) {
+			    	System.out.println("Employee is absent");
+			    }
+			    else if (empCheck == 1) {
+			    	System.out.println("Employee is part time");
+			    }
+			    else {
+			    	System.out.println("Employee is full time");
+			    }
+			    empHours = empHours(empCheck);
+			    empWage = empWage(empHours);
+			    totalempHours += empHours;   // a += b     a = a+b
+			    totalEmpSalary += empWage;
+			    System.out.println("Daily Wage for day " + day + " = " + totalEmpSalary + "\n");
+			    day++;
+			}
+			System.out.println("\nTotal working hours = " + totalempHours);
+			return totalEmpSalary;
+		    }
 }
+
+
 
 
 
---
+++ src/com/bridgelabz/EmployeeWageMainClass.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class EmployeeWageMainClass {
+	public static void main(String[] arg) {
+		int totalEmpSalary;
+		totalEmpSalary = EmployeeWage.totalEmpSalary();
+		System.out.println("Total Wage = " + totalEmpSalary);
+	}
+}
